a13624bb8d637f1b1a48b68d490ede315ca0050a
Sebastian Krome <sebastian.krome@cgi.com>
2017-11-02 21:50:02 +0100

    ServiceDiscovery hinzugefuegt

--- src/main/java/com/sebkro/keyvalue/KeyvalueApplication.java
+++ src/main/java/com/sebkro/keyvalue/KeyvalueApplication.java
@@ -2,8 +2,10 @@ package com.sebkro.keyvalue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class KeyvalueApplication {
 
 	public static void main(String[] args) {
